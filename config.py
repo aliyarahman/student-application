4268f3d1904739371437efb98e7d242ab0ba52a0
Aliya Rahman <aliya.rahman@yahoo.com>
2013-12-19 13:26:33 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,7 @@
 CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = 'big secret'
 
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
-MAIL_USERNAME = 'aliya@codeforprogress.org'
-MAIL_PASSWORD = 'e19740crsd'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:Popcorn13@localhost/studentapp'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:Popcorn13@localhost/studentapp'
 
 ADMINS = ['aliya@codeforprogress.org']
