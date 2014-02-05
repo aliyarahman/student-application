e3945739bb39e1fa1f7d2f7c062db3f34a7db866
Aliya Rahman <aliya.rahman@yahoo.com>
2014-02-04 22:13:53 -0500

    Delete alans_list.py

--- data-manipulation/alans_list.py
+++
@@ -1,7 +0,0 @@
-from app import db, models
-import csv
-
-finished = models.User.query.filter_by(application_complete = 1).all()
-for_alan = csv.writer(open('for_alan.csv', 'wb'), delimiter=',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
-for f in finished:
-	for_alan.writerow([f.user_id, f.lastname,f.firstname])
